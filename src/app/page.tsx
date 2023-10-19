a275f646fbbf7e1d53738b73bfe2b9836bdc2c9b
FNYukii <forestgreen14689@gmail.com>
2023-10-19 14:00:35 +0900

    feat: Aboutページへ遷移できた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
 			<h1>Hello Next App Router</h1>
+			<Link href="/about" className="hover:underline">About</Link>
 		</main>
   )
 }
